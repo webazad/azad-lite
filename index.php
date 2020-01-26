6d164b5b9f74315ea78a9f58eebf18294dea4c84
Azad <azad@azad.com>
2020-01-26 20:30:13 +0600

    A bit

--- index.php
+++ index.php
@@ -5,15 +5,18 @@
 * :: @version 1.0.0
 *-------------------------------------------------------------------------------------------------
  */
-?>
-<?php get_header(); ?>
-<?php get_template_part('slug','name'); ?>
+
+get_header();
+
+get_template_part('slug','name'); ?>
 <!-- SPECIAL SECTION BEGINS -->
 <section class="azad-blog">	
 	<div class="azad-container">
 		<div class="blog-container">
-			<?php get_template_part('loop'); ?>
-			<?php get_sidebar(); ?>
+			<?php 
+				get_template_part('loop','index'); 
+				get_sidebar();
+			?>
 		</div>
 	</div>
 </section><!-- ends special section -->
--- single.php
+++ single.php
@@ -5,14 +5,15 @@
 * :: @version 1.0.0
 *-------------------------------------------------------------------------------------------------
  */
-?>
-<?php get_header(); ?>
-<?php get_template_part('slug','name'); ?>
+ 
+get_header();
+get_template_part('slug','name'); ?>
+
 <!-- SPECIAL SECTION BEGINS -->
 <section class="azad-blog">	
 	<div class="azad-container">
 		<div class="blog-container">
-			<?php get_template_part('loop'); ?>
+			<?php get_template_part('loop','single'); ?>
 			<?php get_sidebar(); ?>
 		</div>
 	</div>
