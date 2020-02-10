b4377552d3d9324f1e19e77c3add167c2fafdb4d
Azad <azad@azad.com>
2020-02-10 10:56:12 +0600

    Meta

--- functions.php
+++ functions.php
@@ -23,3 +23,29 @@ endif;
 
 require_once AZAD_LITE_DIR . '/inc/azad-hooks.php';
 
+if(! function_exists('azad_posted_on')){
+	function azad_posted_on(){
+		printf(__(
+			'<span class="%1$s">Posted on </span> %2$s <span class="meta-sep">by</span> %3$s','azad-x'),
+			'meta-prep meta-prep-author',
+			sprintf(
+				'<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
+				get_permalink(),
+				esc_attr(get_the_time()),
+				get_the_date()
+			),
+			sprintf(
+				'<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
+				get_author_posts_url( get_the_author_meta( 'ID' ) ),
+				esc_attr( sprintf( __( 'View all posts by %s', 'azad-x' ), get_the_author() ) ),
+				get_the_author()
+			)
+		);
+	}
+}
+if(! function_exists('azad_posted_in')){
+	function azad_posted_in(){
+		echo 'asdf';
+	}
+}
+
--- loop.php
+++ loop.php
@@ -5,12 +5,15 @@
 				<div class="azad-thumb">
 					<?php the_post_thumbnail(); ?>
 				</div>
-				<div class="azad-thumb">
-					<?php the_content(); ?>                                
-				</div>
-				<div class="azad-thumb">
+				<div class="azad-title">
 					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				</div>
+				<div class="entry-meta">
+					<?php azad_posted_on(); ?>
+				</div><!-- .entry-meta -->
+				<div class="azad-content">
+					<?php the_content(); ?>                                
+				</div>				
 			</article>                        
 		<?php endwhile; ?>
 	<?php endif; ?>
