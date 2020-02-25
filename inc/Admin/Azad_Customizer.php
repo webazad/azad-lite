0f8f922ff8b1c1571d1d4b216f10547db4ebbf92
Azad <azad@azad.com>
2020-02-26 05:31:10 +0600

    Preloader

--- inc/Admin/Azad_Customizer.php
+++ inc/Admin/Azad_Customizer.php
@@ -10,6 +10,7 @@ namespace Inc\Admin;
 
 // EXIT IF ACCESSED DIRECTLY
 defined('ABSPATH') || exit;
+
 if ( ! class_exists( 'Azad_Customizer' ) ):
     class Azad_Customizer{
         private static $_instance;
@@ -23,12 +24,21 @@ if ( ! class_exists( 'Azad_Customizer' ) ):
             $this->azad_add_controls( $wp_customize );         
         }
         public function azad_add_panels( $wp_customize ) {
+            // GLOBAL PANEL
             $wp_customize->add_panel('global_panel',array(
                 'title'             => __('Global Settings','azad-lite'),
                 'description'       => 'Globals',
-                'priority'          => 1,
+                'priority'          => 21,
                 'capability'        => 'edit_theme_options'
             ));
+            // HEADER PANEL
+            $wp_customize->add_panel('header_panel',array(
+                'title'             => __('Header Panel','azad-lite'),
+                'description'       => 'Header ...',
+                'priority'          => 100,
+                'capability'        => 'edit_theme_options'
+            ));
+            // FOOTER PANEL
             $wp_customize->add_panel('footer_panel',array(
                 'title'             => __('Footer Panel','azad-lite'),
                 'description'       => 'Footer',
@@ -37,37 +47,71 @@ if ( ! class_exists( 'Azad_Customizer' ) ):
             ));
         }
         public function azad_add_sections( $wp_customize ) {
-            $wp_customize->add_section( 'global_section', array(
-                'title'             => __( 'Base Colors', 'azad-lite' ),
-                'priority'          => 10,
-                'description'       => 'Write something here...', 
-                'priority'          => 11,
+            // PRELOADER SECTION
+            $wp_customize->add_section( 'preloader_section', array(
+                'title'             => __( 'Activate Preloader', 'azad-x' ),
+                'description'       => 'Preloader loads before sites load entirely. Sometimes you can show varius preloader to your traffics.', 
+                'priority'          => 5,
                 'panel'             => 'global_panel',
                 'capability'        => 'edit_theme_options'
             ) );
+            // SEARCH ICON SECTION
+            $wp_customize->add_section( 'search_section', array(
+                'title'             => __( 'Enable search icon', 'azad-x' ),
+                'description'       => 'Enable search ion ...', 
+                'priority'          => 5,
+                'panel'             => 'global_panel',
+                'capability'        => 'edit_theme_options'
+            ) );
+            // BASECOLOR SECTION
+            $wp_customize->add_section( 'global_section', array(
+                'title'             => __( 'Base Colors', 'azad-lite' ),
+                'description'       => 'Write something here...', 
+                'priority'          => 16,
+                'panel'             => 'global_panel',
+                'capability'        => 'edit_theme_options'
+            ) );
+            // HEADER LOGO SECTION
+            $wp_customize->add_section( 'header_logo', array(
+                'title'             => __( 'Header logo', 'azad-lite' ),
+                'description'       => 'Write something here...', 
+                'priority'          => 11,
+                'panel'             => 'header_panel',
+                'capability'        => 'edit_theme_options'
+            ) );
+            // FOOTER LOGO SECTION
             $wp_customize->add_section( 'footer_logo', array(
-                'title'    => __( 'Footer logo', 'azad-lite' ),
-                'priority' => 101,
+                'title'             => __( 'Footer logo', 'azad-lite' ),
                 'description'       => 'Write something here...', 
                 'priority'          => 11,
                 'panel'             => 'footer_panel',
                 'capability'        => 'edit_theme_options'
             ) );
+            // FOOTER TEXT SECTION
 			$wp_customize->add_section( 'footer_text', array(
                 'title'             => __( 'Footer Text', 'azad-lite' ),
-                'priority'          => 101,
                 'description'       => 'All about footer...', 
-                'priority'          => 11,
+                'priority'          => 12,
                 'panel'             => 'footer_panel',
                 'capability'        => 'edit_theme_options'
             ) );
         }
         public function azad_add_settings( $wp_customize ) {
-            // BASE COLORS
+            // PRELOADER SETTING
+            $wp_customize->add_setting( 'preloader_settings', array(
+                'default'           => true,
+                'transport'         => 'refresh',
+            ) );
+            // BASE COLORS SETTING
             $wp_customize->add_setting( 'global_settings', array(
                 'default'           => true,
             ) );
-			$wp_customize->add_setting( 'enable_header_search', array(
+            // SEARCH SETTING
+			$wp_customize->add_setting( 'header_search_icon', array(
+                'default'           => true,
+            ) );
+            // PRELOADER SETTING
+            $wp_customize->add_setting( 'header_logo_image', array(
                 'default'           => true,
             ) );
             $wp_customize->add_setting( 'footer_logo_image', array(
@@ -86,14 +130,30 @@ if ( ! class_exists( 'Azad_Customizer' ) ):
                 'transport'         => 'refresh',
             ) );
         }
-        public function azad_add_controls( $wp_customize ) {
+        public function azad_add_controls( $wp_customize ) {    
+            // GLOBAL CONTROLS        
+            $wp_customize->add_control( new \WP_Customize_Control( $wp_customize, 'enable_preloader', array(
+                'label'             => 'Select background color for footer',
+                'description'       => 'Select color...',        
+                'section'           => 'preloader_section',
+                'settings'          => 'preloader_settings', 
+                'type'              => 'checkbox', 
+            ) ) );
+            // ENABLE HEADER SEARCH ICON
+            $wp_customize->add_control( new \WP_Customize_Control( $wp_customize, 'enable_header_search', array(
+                'label'             => 'Enable header search icon',
+                'description'       => 'Enable it ...',        
+                'section'           => 'search_section',
+                'settings'          => 'header_search_icon', 
+                'type'              => 'checkbox', 
+            ) ) );
             // BASE COLORS
             $wp_customize->add_control( new \WP_Customize_Color_Control( $wp_customize, 'bg_body', array(
                 'label'             => 'Select background color for footer',
                 'description'       => 'Select color...',        
                 'section'           => 'global_section',
                 'settings'          => 'global_settings',    
-            ) ) );
+            ) ) );            
             $wp_customize->add_control( new \WP_Customize_Color_Control( $wp_customize, 'bg', array(
                 'label'             => 'Select background color for footer',
                 'description'       => 'Select color...',        
@@ -136,13 +196,14 @@ if ( ! class_exists( 'Azad_Customizer' ) ):
                 'section'           => 'global_section',
                 'settings'          => 'global_settings',    
             ) ) );
-			$wp_customize->add_control( 'enable_header_search',array(
-					'type'     => 'checkbox',
-					'section'  => 'global_section',
-					'priority' => 10,
-					'label'    => __( 'Show search in header', 'twentytwenty' ),
-				) );
-            // FOOTER LOGO
+			// HEADER CONTROL
+            $wp_customize->add_control( new \WP_Customize_Image_Control( $wp_customize, 'header_featured_image', array(
+                'label'             => 'Upload Your Logo',
+                'description'       => 'Write something here...',        
+                'section'           => 'header_logo',
+                'settings'          => 'header_logo_image',    
+            ) ) );
+            // FOOTER CONTROLS
             $wp_customize->add_control( new \WP_Customize_Image_Control( $wp_customize, 'auto_add_featured_image', array(
                 'label'             => 'Upload Your Logo',
                 'description'       => 'Write something here...',        
@@ -175,7 +236,7 @@ if ( ! class_exists( 'Azad_Customizer' ) ):
             return self::$_instance;
         }
         public function __destruct() {}
-     }
+    }
 endif;
 
 if(! function_exists('load_azad_customizer')){
