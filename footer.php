dcb0a76b4fd3d4a3e4093796664b02481511f70b
Azad <azad@azad.com>
2020-11-12 17:32:25 +0600

    Customizer

--- footer.php
+++ footer.php
@@ -7,59 +7,41 @@
  */
 ?>
             <!-- FOOTER SECTION BEGINS -->
-            <footer class="azad-footer">
+            <footer class="azad-footer" style="background-image:url(<?php echo get_theme_mod( 'footer_bg_image', '#ffffff' ); ?>);background-color:<?php echo get_theme_mod( 'footer_bg_color', '#39e1f5' ); ?>;color:<?php echo get_theme_mod( 'footer_text_color', '#ffffff'); ?>;">
                 <div class="footer-top">
 					<div class="azad-container">
-						<div class="footer-container">
-							<div class="footer-widget-1">
-								<!--  THE WAY SHOW DYNAMIC SIDEBAR -->
-								<?php if ( ! dynamic_sidebar( 'footer_widget_one' ) ) : ?>
-									<aside id="search" class="widget">
-										<p>You need to select a widget to display data. Basically people select logo here.</p>
-									</aside>
-								<?php endif; // end sidebar widget area ?>
-							</div>
-							<nav class="footer-widget-2">
-								<h3>Important Links</h3>
-								<?php
-									if ( function_exists( 'wp_nav_menu' ) ) {
-										$defaults = array(
-											'theme_location'  => 'footer_menu_one',
-											'menu'            => '',
-											'container'       => 'div',
-											'container_class' => '',
-											'container_id'    => '',
-											'menu_class'      => 'menu',
-											'menu_id'         => '',
-											'echo'            => true,
-											'fallback_cb'     => 'wp_page_menu',
-											'before'          => '',
-											'after'           => '',
-											'link_before'     => '',
-											'link_after'      => '',
-											'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-											'depth'           => 0,
-											'walker'          => ''
-										);
-										wp_nav_menu( $defaults );
-									}
-								?>
-							</nav>
-							<div class="footer-widget-3">
-								<!--  THE WAY SHOW DYNAMIC SIDEBAR -->
-								<?php if ( ! dynamic_sidebar( 'footer_widget_two' ) ) : ?>
-									<aside id="search" class="widget">
-										<p>You need to select a widget to display data.</p>
-									</aside>
-								<?php endif; // end sidebar widget area ?>
+							<div class="footer-top-container">
+								<div class="azad-footer-widget">
+									<!--  THE WAY TO SHOW DYNAMIC WIDGET -->
+									<?php if ( ! dynamic_sidebar( 'footer_widget_one' ) ) : ?>
+										<p>Please select a widget to display data.</p>    
+									<?php endif; ?>
+								</div>
+								<div class="azad-footer-widget">
+									<!--  THE WAY TO SHOW DYNAMIC WIDGET -->
+									<?php if ( ! dynamic_sidebar( 'footer_widget_two' ) ) : ?>
+										<p>Please select a widget to display data.</p>    
+									<?php endif; ?>
+								</div>
+								<div class="azad-footer-widget">
+									<!--  THE WAY TO SHOW DYNAMIC WIDGET -->
+									<?php if ( ! dynamic_sidebar( 'footer_widget_three' ) ) : ?>
+										<p>Please select a widget to display data.</p>    
+									<?php endif; ?>
+								</div>
+								<div class="azad-footer-widget">
+								<!--  THE WAY TO SHOW DYNAMIC WIDGET -->
+								<?php if ( ! dynamic_sidebar( 'footer_widget_four' ) ) : ?>
+									<p>Please select a widget to display data.</p>    
+								<?php endif; ?>
 							</div>
 						</div>
 					</div>
 				</div>                
-                <div class="azad-copyright" style="background:<?php echo get_theme_mod( 'copyright_bg', '#39e1f5' ); ?>;color:<?php echo get_theme_mod( 'copyright_color', '#ffffff'); ?>;">
+                <div class="azad-copyright" style="background-color:<?php echo get_theme_mod( 'copyright_bg_color', '#000000' ); ?>;color:<?php echo get_theme_mod( 'copyright_text_color', '#ffffff' ); ?>;">
 					<div class="azad-container">
 						<div class="copyright-container">
-							<p><?php echo get_theme_mod( 'copyright_text', 'Copyright' ); ?> <?php echo date( "Y" ); ?><a href="<?php echo home_url(); ?>"> <?php bloginfo( 'name' ); ?></a></p>
+							<p><?php echo date( "Y" ); ?> <?php echo get_theme_mod( 'copyright_text', 'Write copyright text here...' ); ?> <a href="<?php echo home_url(); ?>"> <?php bloginfo( 'name' ); ?></a></p>
 						</div>
 					</div>
                 </div>
